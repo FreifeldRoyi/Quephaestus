9cf801ccc63a9fee9b98762d394f46ae9cf5c3d1
Royi Freifeld <freifeld.royi@gmail.com>
2025-02-02 15:42:15 +0200

    Fix tests

--- src/main/java/com/freifeld/tools/quephaestus/exceptions/ExceptionHandler.java
+++ src/main/java/com/freifeld/tools/quephaestus/exceptions/ExceptionHandler.java
@@ -2,41 +2,35 @@ package com.freifeld.tools.quephaestus.exceptions;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import picocli.CommandLine;
-import picocli.CommandLine.Help.Ansi;
 
-import static picocli.CommandLine.Help.Ansi.AUTO;
+import java.io.PrintWriter;
 
 @ApplicationScoped
 public class ExceptionHandler implements CommandLine.IParameterExceptionHandler, CommandLine.IExecutionExceptionHandler {
 
-    public Ansi.Text toExceptionStyle(String message) {
-        return AUTO.text("@|fg(red) %s|@".formatted(message));
+    private int handleException(Exception exception, PrintWriter out, CommandLine.Help.ColorScheme colorScheme) {
+        var message = exception.getMessage();
+        if (exception.getCause() != null && exception.getCause() instanceof QuephaestusException ex) {
+            message = ex.getMessage();
+        }
+
+        out.println(colorScheme.text(message));
+        return 2;
     }
 
     @Override
     public int handleParseException(CommandLine.ParameterException exception, String[] args) throws Exception {
-        switch (exception.getCause()) {
-            case QuephaestusException ex -> {
-                final var message = this.toExceptionStyle(ex.getMessage());
-                exception.getCommandLine().getOut().println(message);
-            }
-            default -> throw exception;
-        }
-
-        return 2;
+        return this.handleException(exception, exception.getCommandLine().getErr(), exception.getCommandLine().getColorScheme());
     }
 
     @Override
     public int handleExecutionException(Exception exception, CommandLine commandLine, CommandLine.ParseResult fullParseResult) throws Exception {
-        switch (exception) {
-            case QuephaestusException ex -> {
-                final var message = this.toExceptionStyle(ex.getMessage());
-                commandLine.getOut().println(message);
-            }
-            default -> throw exception;
+
+        if (exception instanceof QuephaestusException ex) {
+            return this.handleException(exception, commandLine.getErr(), commandLine.getColorScheme());
         }
 
-        return 2;
+        throw exception;
     }
 
 }
--- src/test/java/com/freifeld/tools/quephaestus/ForgeCommandTest.java
+++ src/test/java/com/freifeld/tools/quephaestus/ForgeCommandTest.java
@@ -5,32 +5,26 @@ import io.quarkus.test.junit.main.QuarkusMainLauncher;
 import io.quarkus.test.junit.main.QuarkusMainTest;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.nio.file.Files;
-
-import static com.freifeld.tools.quephaestus.messages.ExceptionMessageTemplates.PATH_ALREADY_EXISTS;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @QuarkusMainTest
-class ForgeCommandTest
-{
-	private static LauncherParamsBuilder createLaunchParams()
-	{
-		return new LauncherParamsBuilder().withSubCommandName("forge");
-	}
+class ForgeCommandTest {
+    private static LauncherParamsBuilder createLaunchParams() {
+        return new LauncherParamsBuilder().withSubCommandName("forge");
+    }
 
-	@Test
-	public void testForgeCommand_elementNameDoesNotExist(QuarkusMainLauncher launcher)
-	{
-		var element = "controller";
-		var launchParams = createLaunchParams().withConfigPath().withElement(element).withModule("my-controller");
-		var buildArgs = launchParams.build();
-		var result = launcher.launch(buildArgs);
-		assertEquals(2, result.exitCode());
-		assertTrue(result.getErrorOutput().startsWith(ExceptionMessageTemplates.INVALID_ELEMENT.formatted(
-				element,
-				"[resource, resource-mapper, orchestrator, repository, entity, create-command]",
-				launchParams.configFilePath.getPath())));
-	}
+    @Test
+    public void testForgeCommand_elementNameDoesNotExist(QuarkusMainLauncher launcher) {
+        var element = "controller";
+        var launchParams = createLaunchParams().withConfigPath().withElement(element).withModule("my-controller");
+        var buildArgs = launchParams.build();
+        var result = launcher.launch(buildArgs);
+        assertEquals(2, result.exitCode());
+        assertTrue(result.getErrorOutput().startsWith(ExceptionMessageTemplates.INVALID_ELEMENT.formatted(
+                element,
+                "[resource, resource-mapper, orchestrator, repository, entity, create-command]",
+                launchParams.configFilePath.getPath())));
+    }
+
 }
